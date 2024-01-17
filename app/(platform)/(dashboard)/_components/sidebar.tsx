8b3503d16c67868b9a782cc24c7b15b3dfdf9ff9
Prashanth Balakrishnan <prashbala13@gmail.com>
2024-01-17 14:50:51 -0500

    added layouts

---
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+interface SidebarProps {
+  storageKey?: string;
+}
+
+export const SideBar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  return <div>sidebar</div>;
+};
